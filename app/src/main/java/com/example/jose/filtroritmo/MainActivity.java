bfd11f7088feed31fd4f576532960a91f990a83f
ReXiges <sergie98@gmail.com>
2017-09-07 22:18:19 -0600

    Mensajes Beta

--- app/src/main/java/com/example/jose/filtroritmo/MainActivity.java
+++ app/src/main/java/com/example/jose/filtroritmo/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.jose.filtroritmo;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,7 +20,8 @@ import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
-
+    private final int SELECT_IMAGE =2;
+    private Bitmap imageSelected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,11 +42,22 @@ public class MainActivity extends AppCompatActivity {
             Bundle extras = data.getExtras();
             Bitmap fotoTomada= (Bitmap) extras.get("data");
         }
+        if(requestCode==SELECT_IMAGE && resultCode == RESULT_OK && data!=null){
+            Uri imagenElegida = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(imagenElegida , filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String filePath = cursor.getString(columnIndex);
+            cursor.close();
+            Bitmap yourSelectedImage  = BitmapFactory.decodeFile(filePath);
+        }
     }
 
     public void botonGaleria(View view){
         Intent openGalleryIntent = new Intent (Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
         openGalleryIntent.setType("image/*");
         openGalleryIntent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(openGalleryIntent, "Select Picture"),SELECT_IMAGE);
     }
 }
