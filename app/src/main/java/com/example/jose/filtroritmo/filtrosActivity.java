9024575dd0fe578cbd79935a556e3cd230131d03
jose51197 <jose51197@hotmail.com>
2017-09-14 20:57:59 -0600

    antes de que se lo vuele sergie

--- app/src/main/java/com/example/jose/filtroritmo/filtrosActivity.java
+++ app/src/main/java/com/example/jose/filtroritmo/filtrosActivity.java
@@ -2,11 +2,14 @@ package com.example.jose.filtroritmo;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+
+import java.nio.ByteBuffer;
 
 public class filtrosActivity extends AppCompatActivity {
     Bitmap imagen;
@@ -20,10 +23,10 @@ public class filtrosActivity extends AppCompatActivity {
     public void getImagen(){
         Intent intent=getIntent();
         this.imagen = (Bitmap) intent.getParcelableExtra("imagen");
-        setImagen(this.imagen);
+        setImagen();
     }
     //setea imagenes filtradas en el widget
-    public void setImagen(Bitmap imagen){
+    public void setImagen(){
         ImageView imagensita = (ImageView)findViewById(R.id.imageView);
         imagensita.setImageBitmap(this.imagen);
     }
@@ -31,7 +34,35 @@ public class filtrosActivity extends AppCompatActivity {
     }
     public void dm(View view){
     }
+    //el mas facil y con el cual empezar
     public void average(View view){
+        Bitmap filtrada=this.imagen.copy(this.imagen.getConfig(),true);
+        int altura = filtrada.getHeight();
+        int ancho  = filtrada.getWidth();
+        int actual;
+        byte [] bytes;
+        for (int i=0;i<altura;i++){
+            for (int j=0;j<ancho;j++){
+                try{
+                    actual=filtrada.getPixel(i,j);
+                    bytes= ByteBuffer.allocate(4).putInt(actual).array();
+                    for(byte b : bytes){
+                        actual+=b;
+                    }
+                    actual=actual/3;
+                    actual= (actual & 0xff) << 24 | (actual & 0xff) << 16 | (actual & 0xff) << 16 | (actual & 0xff);
+
+                    filtrada.setPixel(i,j,actual);
+                }
+                catch(Exception e){
+                    System.out.println("crash en"+String.valueOf(i)+","+String.valueOf(j));
+                }
+
+            }
+        }
+        System.out.println("Logrado");
+        this.imagen=filtrada;
+        setImagen();
     }
     public void desaturation(View view){
     }
